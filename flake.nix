7231f9578d310e7ff7eca5b13e469a054d710957
creatifcoding <the@getbygenius.com>
2025-11-13 05:18:26 -0500

    chore(nix): using flake-parts properly.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,44 @@
 {
-  description = "A very basic flake";
+  description = "GETBYGENIUS AFFAIRS FLAKE.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    fenix = {
+      url = "https://flakehub.com/f/nix-community/fenix/0.1.*.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-linter = {
+      url = "github:mic92/flake-linter";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+        specialArgs = { };
+      }
+      {
+        imports = [ ./nix ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+        ];
+      };
 }
