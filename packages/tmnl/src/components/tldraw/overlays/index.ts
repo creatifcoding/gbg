49e88a26c0923dca765fd8dbeb6b34674e828bfa
creatifcoding <the@getbygenius.com>
2025-11-29 20:39:45 -0500

    atp, i'm just getting it done.

---
+++ packages/tmnl/src/components/tldraw/overlays/index.ts
@@ -0,0 +1,8 @@
+/**
+ * tldraw Overlay Components
+ *
+ * Custom overlay components for enhanced drag/resize interactions.
+ */
+
+export { DragReticleOverlay, ShapeEmanation } from './DragReticleOverlay'
+export { ReticleHandles } from './ReticleHandles'
