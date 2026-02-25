4b8335429c09560a6124098f94cf0dadf5707622
creatifcoding <the@getbygenius.com>
2026-02-25 05:56:27 -0500

    chore(ava-elixir): set repo pg baseline and suppress ash-functions warning

--- packages/tmnl/ava-elixir/lib/ava_elixir/repo.ex
+++ packages/tmnl/ava-elixir/lib/ava_elixir/repo.ex
@@ -2,5 +2,9 @@ defmodule AvaElixir.Repo do
   @moduledoc false
 
   use AshPostgres.Repo,
-    otp_app: :ava_elixir
+    otp_app: :ava_elixir,
+    warn_on_missing_ash_functions?: false
+
+  @spec min_pg_version() :: Version.t()
+  def min_pg_version, do: %Version{major: 13, minor: 0, patch: 0}
 end
